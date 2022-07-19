0035456e51298b70c371a39de7532a495b361cab
Jintao Huang <hjt_study@qq.com>
2022-07-19 14:52:45 +0800

    commit

--- tutorials/pl/pre.py
+++ tutorials/pl/pre.py
@@ -1,8 +1,11 @@
 import sys
-sys.path.append("/home/jintao/Desktop/coding/python/ml_alg")
-from libs import *
-# 
+import os
 _ROOT_DIR = "/home/jintao/Desktop/coding/python/ml_alg"
+if not os.path.isdir(_ROOT_DIR):
+    raise IOError(f"_ROOT_DIR: {_ROOT_DIR}")
+sys.path.append(_ROOT_DIR)
+from libs import *
+
 RUNS_DIR = os.path.join(_ROOT_DIR, "runs")
 os.makedirs(RUNS_DIR, exist_ok=True)
 PL_RUNS_DIR = os.path.join(RUNS_DIR, "PL")
